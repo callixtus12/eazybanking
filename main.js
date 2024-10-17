990853f57ae851a1de9c0d47dad6b858fc7c2910
callixtus12 <callistusedoziuno@gmail.com>
2024-10-17 12:13:01 +0100

    initail

---
+++ main.js
@@ -0,0 +1,15 @@
+const menu = document.querySelector(".ri-menu-line")
+const close = document.querySelector(".ri-close-line")
+const nav = document.querySelector("nav ul")
+
+menu.addEventListener('click', ()=>{
+    nav.style.top = '30%'
+    menu.style.display = 'none'
+    close.style.display = 'block'
+})
+
+close.addEventListener('click', ()=>{
+    nav.style.top = '-30%'
+    menu.style.display = 'block'
+    close.style.display = 'none'
+})
